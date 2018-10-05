c3347e79b700c198ca3cbabb8d144c5ad4508efd
Muhanned Abdulhaq Mohammed Noman <muhanned.noman@gmail.com>
2018-10-05 13:22:34 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
- import React, { Component } from 'react' // We need to import react so we can make use of its .component class
+import React, { Component } from 'react' // We need to import react so we can make use of its .component class
 import ReactDOM from 'react-dom' // ...and we need to import ReactDOM so we can create and test a virtual DOM with react!
 
 const BUTCHER_PRODUCTS = [
